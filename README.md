5e1f96f10438b9a22d7e9fa8ea613bfaf43a6297
Kevin Siml <appzer@users.noreply.github.com>
2017-03-07 17:43:47 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -6,10 +6,13 @@
 This app lets you send [pushsafer.com](https://www.pushsafer.com/) notifications to use in flows on a Homey device (by Athom).
 
 1. Go to [pushsafer.com](https://www.pushsafer.com/) and login
-2. Grab your user key or alias key (with predefined device, sound, icon & vibration) from the dashboard
+2. Grab your private key (send to all registered devices) or alias key (with predefined device, sound, icon & vibration) from the dashboard
 3. Go to settings on your Homey, and under Pushsafer Notifications fill in your private or alias key and save.
 
 ### Changelog
+
+0.0.3
+- fixed some issues
 
 0.0.2
 - fixed some issues
--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var push = require('pushsafer-notifications');
 var http = require('http.min');
 var account = [];
 var request = [];
-var devices = null;
 var pushsaferUser = null;
 var ledringPreference = false;
 
@@ -28,22 +27,6 @@ Homey.manager('flow').on('action.pushsaferSend', function( callback, args ){
 		if( typeof pMessage == 'undefined' ||pMessage == null || pMessage == '') return callback( new Error("Message can not be empty") );
 		pushsaferSend ( tempUser, pMessage);
     callback( null, true ); // we've fired successfully
-});
-
-Homey.manager('flow').on('action.pushsaferSend_device', function( callback, args ){
-	  var tempUser = pushsaferUser;
-		var pMessage = args.message;
-		if( typeof pMessage == 'undefined' ||pMessage == null || pMessage == '') return callback( new Error("Message can not be empty") );
-		var pDevice = args.device.name;
-		if( pDevice == null || pDevice == '') return callback( new Error("No devices registered on this Pushsafer account!") );
-		pushsaferSend_device ( tempUser, pMessage, pDevice);
-    callback( null, true ); // we've fired successfully
-});
-
-Homey.manager('flow').on('action.pushsaferSend_device.device.autocomplete', function( callback, value ) {
-	var deviceSearchString = value.query;
-	var items = searchForDevicesByValue( deviceSearchString );
-	callback( null, items );
 });
 
 
@@ -81,52 +64,6 @@ function pushsaferSend ( pUser, pMessage) {
 		}
 	}
 }
-
-// Send notification with parameters
-function pushsaferSend_device ( pUser, pMessage, pDevice) {
-
-	if (pUser != ""){
-	var p = new push( {
-		k: pUser
-	});
-
-	var msg = {
-		// These values correspond to the parameters detailed on https://pushsafer.net/api
-		// 'message' is required. All other values are optional.
-		message: pMessage,   // required
-		title: "Homey",
-		device: pDevice
-	};
-
-	p.send( msg, function( err, result ) {
-		if ( err ) {
-			throw err;
-		} else {
-			if (ledringPreference == true){
-				LedAnimate("green", 3000);
-			}
-		}
-		Homey.log( result );
-	});
-	} else {
-		if (ledringPreference == true){
-			LedAnimate("red", 3000);
-		}
-	}
-}
-
-function searchForDevicesByValue ( value ) {
-	var possibleDevices = devices;
-	var tempItems = [];
-	for (var i = 0; i < devices.length; i++) {
-		var tempDevice = possibleDevices[i];
-		if ( tempDevice.indexOf(value) >= 0 ) {
-			tempItems.push({ icon: "", name: tempDevice });
-		}
-	}
-	return tempItems;
-}
-
 
 function LedAnimate(colorInput, duration) {
 Homey.manager('ledring').animate(
